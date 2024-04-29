1aca1cb86e3be369b9621e5fc3731797a80eef5b
yuna-hh <ghddbsk12@naver.com>
2024-04-29 16:53:47 +0900

    수정중

--- movie.js
+++ movie.js
@@ -7,16 +7,13 @@ const options = {
 };
 
 // 카드 
-
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
   .then(response => response.json())
   .then(response => {
-    const movies = response.results;
+    let movies = response.results;
     let innerHTML = '';
 
     movies.forEach(movie => {
-      console.log('movie', movie);
-
       //li 불러오기
       innerHTML += `<li id=${movie.id} class="card"><img src=http://image.tmdb.org/t/p/w342${movie.poster_path} alt="movie img"/>
       <div class="rating">
@@ -27,12 +24,37 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
       </li>`
     })
     
-    document.querySelector('#card_list').innerHTML = innerHTML;
+    document.getElementById('card_list').innerHTML = innerHTML;
     let cards = document.querySelectorAll('.card');
     cards.forEach(card =>{
       card.addEventListener('click', function (event) {
       alert(event.currentTarget.id);
+    })});
+
+    document.getElementById('search').addEventListener('submit', function (event) {
+      event.preventDefault();
+      const searchQuery = document.getElementById('searchInput').value.toLowerCase();
+      const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(searchQuery))
+      let filteredHTML = '';
+      filteredMovies.forEach(movie => {
+        //li 불러오기
+        filteredHTML += `<li id=${movie.id} class="card"><img src=http://image.tmdb.org/t/p/w342${movie.poster_path} alt="movie img"/>
+        <div class="rating">
+        <span>${movie.title}</span>
+        <span>${movie.vote_average}</span>
+        <p>${movie.overview}</p>
+        </div>
+        </li>`
+      })
+      document.getElementById('card_list').innerHTML = filteredHTML;
+      let cards = document.querySelectorAll('.card');
+      cards.forEach(card =>{
+        card.addEventListener('click', function (event) {
+        alert(event.currentTarget.id);
+      })});
     });
-    })
+
   })
-  .catch(err => console.error(err));
+
+  
+
